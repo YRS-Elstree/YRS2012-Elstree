6ebcd7c5968624a81a2a95881ac8c99bb4f0aae8
Luke <luke_a_ashford@yahoo.co.uk>
2012-08-10 18:48:51 +0100

    Bug fixing. Again

--- statistics.js
+++ statistics.js
@@ -2,7 +2,7 @@ function generateGraphs(map) {
 	console.log("Starting grpahs");
 	var totalCrimes = 0;
 	for(var cat in map){
-		console.log("Graphs: "+cat+"   "+ca[count]);
+		console.log("Graphs: "+cat+"   "+cat[count]);
 		totalCrimes += cat.count;
 	}
 	console.log("Total Crimes: "+totalCrimes);
