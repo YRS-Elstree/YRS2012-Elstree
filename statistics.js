20cc2fbcc9e6fa80d14236ceb7b2db9b01fda7fe
Luke <luke_a_ashford@yahoo.co.uk>
2012-08-10 19:13:00 +0100

    This might even work...

--- statistics.js
+++ statistics.js
@@ -2,11 +2,9 @@ function generateGraphs(map) {
 	console.log("Graphs: "+map);
 	console.log("Starting grpahs");
 	
-	alert(map[0]);
-	
 	var totalCrimes = 0;
 	for(var cat in map){
-		console.log("Graphs: "+cat+"   "+cat.count);
+		console.log("Graphs: "+map[cat].count);
 		totalCrimes += cat.count;
 	}
 	console.log("Total Crimes: "+totalCrimes);
