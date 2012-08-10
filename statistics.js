fd0c344b0ae3f48784d49a711245b2cb86950faf
Luke <luke_a_ashford@yahoo.co.uk>
2012-08-10 18:18:34 +0100

    Debugging

--- statistics.js
+++ statistics.js
@@ -1,6 +1,7 @@
 function generateGraphs(map){
 	var totalCrimes = 0;
 	for(cat in map){
+		console.log("Graphs: "+cat+"   "+cat.count);
 		totalCrimes += cat.count;
 	}
 	console.log("Total Crimes: "+totalCrimes);
