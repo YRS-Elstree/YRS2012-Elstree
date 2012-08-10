7158b721b8aef712f4da68fc6f0f31fac9f4a9ae
Luke <luke_a_ashford@yahoo.co.uk>
2012-08-10 19:37:13 +0100

    Just a test

--- statistics.js
+++ statistics.js
@@ -1,11 +1,16 @@
 function generateGraphs(map) {
-	console.log("Graphs: "+map);
-	console.log("Starting grpahs");
-	
 	var total = getTotal(map);
+	
+	alert(map[0]);
 	
 	var percentages = calculatePercentages(map, total);
 	console.log("Percentages: "+percentages);
+	
+	/*
+	var output = 
+	"<table>"+
+	"<tr><td>"+map[0]+"</td></tr>";
+	*/
 }
 
 function getTotal(map){
@@ -25,7 +30,6 @@ function calculatePercentages(map, total){
 	for(var i = 0; i < percentages.length; i++){
 		var percent = (map[i].count / total) * 100
 		percentages[i] = Math.round(percent);
-		console.log("percent "+percent);
 	}
 	
 	return percentages;
